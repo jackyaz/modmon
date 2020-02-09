bec482ff23cfdffe0689f409320be7006726e795
Jack <jack.naisbett@gmail.com>
2020-02-09 00:00:07 +0000

    Add AMTM tab

--- modmon.sh
+++ modmon.sh
@@ -368,11 +368,13 @@ Mount_WebUI(){
 	
 	if ! grep -q 'menuName: "Addons"' /tmp/menuTree.js ; then
 		lineinsbefore="$(( $(grep -n "exclude:" /tmp/menuTree.js | cut -f1 -d':') - 1))"
-		sed -i "$lineinsbefore"'i,\n{\nmenuName: "Addons",\nindex: "menu_Addons",\ntab: [\n{url: "NULL", tabName: "__INHERIT__"}\n]\n}' /tmp/menuTree.js
+		sed -i "$lineinsbefore"'i,\n{\nmenuName: "Addons",\nindex: "menu_Addons",\ntab: [\n{url: "amtmredirect.htm", tabName: "Learn about AMTM"}\n{url: "NULL", tabName: "__INHERIT__"}\n]\n}' /tmp/menuTree.js
 	fi
 	
-	lineappafter="$(( $(grep -n 'index: "menu_Addons"' /tmp/menuTree.js | cut -f1 -d':') + 1))"
-	sed -i "$lineappafter""a{url: \"$MyPage\", tabName: \"Modem Monitoring\"}," /tmp/menuTree.js
+	#lineappafter="$(( $(grep -n 'index: "menu_Addons"' /tmp/menuTree.js | cut -f1 -d':') + 1))"
+	
+	sed -i "/url: \"amtmredirect.htm\", tabName:/a {url: \"$MyPage\", tabName: \"Modem Monitoring\"}," /tmp/menuTree.js
+	#sed -i "$lineappafter""a{url: \"$MyPage\", tabName: \"Modem Monitoring\"}," /tmp/menuTree.js
 	umount /www/require/modules/menuTree.js 2>/dev/null
 	mount -o bind /tmp/menuTree.js /www/require/modules/menuTree.js
 }
