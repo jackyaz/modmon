c28e7bd426c2729b415b248398276128cb23410c
Jack <jack.naisbett@gmail.com>
2020-11-07 14:53:30 +0000

    Change update timer in WebUI to 10s down from 20s

--- modmonstats_www.js
+++ modmonstats_www.js
@@ -632,7 +632,7 @@ function CheckUpdate(){
 function DoUpdate(){
 	var action_script_tmp = "start_modmondoupdate";
 	document.form.action_script.value = action_script_tmp;
-	var restart_time = 20;
+	var restart_time = 10;
 	document.form.action_wait.value = restart_time;
 	showLoading();
 	document.form.submit();
