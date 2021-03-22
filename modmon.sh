0554d95f62eadacba016bc4b5205f19bb21ff40c
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-22 21:08:50 +0000

    Reduce version number

--- modmon.sh
+++ modmon.sh
@@ -20,7 +20,7 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="modmon"
-readonly SCRIPT_VERSION="v1.2.0"
+readonly SCRIPT_VERSION="v1.1.3"
 SCRIPT_BRANCH="develop"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME.d"
