0ca5a48016bfcfd51d484240879e4ce6b39f1970
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-23 23:24:13 +0000

    Increase cookie expiry to 10 years

--- modmonstats_www.js
+++ modmonstats_www.js
@@ -529,7 +529,7 @@ function GetCookie(cookiename,returntype){
 }
 
 function SetCookie(cookiename,cookievalue){
-	cookie.set("mod_"+cookiename, cookievalue, 31);
+	cookie.set("mod_"+cookiename, cookievalue, 10 * 365 * 24 * 60 * 60);
 }
 
 $j.fn.serializeObject = function(){
