c11e7026689ae721af78dda7764360d0d562961f
Jack Naisbett <jack.naisbett@gmail.com>
2021-04-24 22:09:55 +0100

    Improve webui settings save

--- modmon.sh
+++ modmon.sh
@@ -273,7 +273,7 @@ Conf_FromSettings(){
 			while IFS='' read -r line || [ -n "$line" ]; do
 				SETTINGNAME="$(echo "$line" | cut -f1 -d'=' | awk '{ print toupper($1) }')"
 				SETTINGVALUE="$(echo "$line" | cut -f2 -d'=')"
-				sed -i "s/$SETTINGNAME=.*/$SETTINGNAME=$SETTINGVALUE/" "$SCRIPT_CONF"
+				sed -i "s~$SETTINGNAME=.*~$SETTINGNAME=$SETTINGVALUE~" "$SCRIPT_CONF"
 			done < "$TMPFILE"
 			grep 'modmon_version' "$SETTINGSFILE" > "$TMPFILE"
 			sed -i "\\~modmon_~d" "$SETTINGSFILE"
